1318d0e99a57db80e7da37631eab9f29c10d783b
luksadmin <lukszwei2@gmail.com>
2020-12-29 21:28:43 -0300

    2020/12/29

---
+++ DJANGO/d/dj-database-url.py
@@ -0,0 +1,30 @@
+
+
+"""
+Objetivo:
+         Configurar um projeto com deploy (Heroku), interagir com um bdd no modo desenvolvimento
+"""
+
+def fonte():
+    """
+    Curso: Programação Web com Python e Django Framework: Essencial
+    Aula: 56. Publicando na internet seu terceiro projeto Django
+    Intervalo: 05:20 até 06:50
+    """
+
+def suporte():
+    """
+    PostgreSQL, PostGIS, MySQL, MySQL (GIS), Oracle, Oracle (GIS), and SQLite
+    """
+
+def terminal():
+    """
+    pip install dj-database-url
+    pip freeze > requirements.txt
+    """
+
+def settings():
+    """
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+    """
---
+++ DJANGO/r/runtime.txt.py
@@ -0,0 +1,29 @@
+
+
+"""
+Objetivo:
+         coletar e armazenar a versão do Python que está sendo executada em um projeto, usado em deploy também
+"""
+
+def fonte():
+    """
+    Curso # Programação Web com Python e Django Framework: Essencial
+    Aula  # 56: Publicando na internet seu terceiro projeto Django
+    Tempo # 08:27 até 09:00
+    """
+
+# Copiar a parte numérica retorno
+def terminal():
+    """ python -V / python --version """
+
+def raiz():
+    """ botão direito / new / file / runtime.txt """
+
+# Colar a parte numérica do retorno
+def modulo():
+    """
+    python- + ctrl + v do retorno
+    """
+
+def exemplo_resultado():
+    """ python-3.8.5 """
