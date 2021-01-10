3dc9b3f079fe58dd82025fb41383b09d2eb696df
luksadmin <lukszwei2@gmail.com>
2021-01-10 09:27:29 -0300

    2021/01/10

---
+++ pyup_reqs.py
@@ -0,0 +1,45 @@
+
+def checar_dependencias(dependencia_projeto, dependencia_pyup):
+
+    # Dependências atualmente no projeto
+    with open(dependencia_projeto, 'r') as doc:
+        var = sorted(list(str(doc.read()).split('\n')), reverse=True)
+
+    # Dependências atualizadas do projeto, pegas do site Pyup e organizadas em um módulo .txt
+    with open(dependencia_pyup, 'r') as doc:
+        var2 = sorted(list(str(doc.read()).split('\n')), reverse=True)
+
+    "OBS"  # sorted está sendo usado para ordenar dados para evitar possíveis irregularidades do projeto ou do website Pyup
+
+    var3 = ['pip uninstall', ]  # lista de índices que receberá pacote desatualziado a ser desinstalado
+    var4 = ['pip install', ]    # lista de índices que receberá cada pacote atualizado a ser instalado
+
+    "Objetivo"  # Se var e var2 são iguais, não há o que atualizar, então apenas imprime-se 'OK' + incremento
+    "Objetivo"  # Se não, adicionar em var3, o pacote a ser desintalado, e em var4, o pacote a ser instalado + incremento
+    index = 0
+    while index < len(var):
+        if var[index] == var2[index]:
+            print('OK', end=' ')
+            index += 1
+        else:
+            var3.append(var[index])
+            var4.append(var2[index])
+            index += 1
+
+    print('\n')
+    print('Pacotes desatualizados')
+    for obj in var3:
+        print(obj, end=' ')  # Copia-se o retorno dessa linha e execute ela no terminal do projeto
+
+    print('\n')
+    print('Pacotes atualizados')
+    for obj in var4:
+        print(obj, end=' ')  # Depois, copia-se o retorno dessa linha e execute ela no terminal do projeto
+
+checar_dependencias(
+    '/home/lucas/PycharmProjects/recursos/PYTHON/POO/requirements.txt',
+    '/home/lucas/PycharmProjects/recursos/pyup_reqs.txt'
+)
+
+# pip uninstall typed-ast==1.4.1 soupsieve==2.0.1 pytz==2020.4 pytube==10.0.0 pynput==1.7.1 pyinstaller-hooks-contrib==2020.10 image==1.5.33 Pillow==8.0.1 Django==3.1.3
+# pip install typed-ast==1.4.2 soupsieve==2.1 pytz==2020.5 pytube==10.4.1 pynput==1.7.2 pyinstaller-hooks-contrib==2020.11 image==1.5.32 Pillow==8.1.0 Django==3.1.5
